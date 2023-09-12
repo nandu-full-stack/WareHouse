27923409f2d030edbc78e41b33fd82d00844cec8
nandu-full-stack <nandurayapureddy7251@gmail.com>
2023-09-12 09:41:06 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import WareHouseList from './component/wareHouseList';
+import Update from './component/Update';
+import Read from './component/Read';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<WareHouseList/>}></Route>
+        <Route path='/update/:id' element={<Update/>}></Route>
+        <Route path='/read/:id' element={<Read/>}></Route>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
